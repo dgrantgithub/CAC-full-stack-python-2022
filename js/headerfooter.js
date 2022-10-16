a505951d7a809b813e5ec9af1ad9219ccefd96f9
cynthiaaudisio <cpaudisio@gmail.com>
2022-10-16 00:14:29 -0300

    breakpoints

--- js/headerfooter.js
+++ js/headerfooter.js
@@ -23,19 +23,22 @@ let footer = `
     <div class="container-pie">
         
         <div class="item0-pie">
-            <h3 style="color: #E9EAEC; padding: 5vw 0vw 0vw 5vw">Unitarios & Federales</h4>    
+            <h3 style="color: #E9EAEC; padding: 1vh 2.5vw">Unitarios & Federales</h4>    
         </div>
             
         <div class="subcontenedor-pie">
                 
-            <div class="item1-pie" style="padding-left: 5vw">
-                <h5>Dirección</h5>
-                <h5>3 de febrero 1852, Caseros, Argentina</h5>
-                <h5>Horario</h5>
-                <h5>12 a 24 hs</h5>
+            <div class="item1-pie" style="padding: 1.5vh 5vw">
+                
+                <div> <h5>Dirección</h5>
+                <h5>3 de febrero 1852, Caseros, Argentina</h5> </div>
+                
+                <div> <h5>Horario</h5>
+                <h5>12 a 24 hs</h5> </div>
+                
                 </div>
 
-            <div class="item2-pie">
+            <div class="item2-pie" style="padding: 1.5vh 5vw">
                 <h5><img src="/img/flaticon/telefono.png" width="5%"> 4321-1234</h5>
                 <h5><img src="/img/flaticon/whatsapp.png" width="5%"> 15-1234-4321</h5>
                 <h5><img src="/img/flaticon/facebook.png" width="5%"> @Unitarios&Federales</h5>
