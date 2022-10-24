4dd8e1cccf18606b36b502970a5a7763f94af4c8
Leonardo <leonardo_gil02@hotmail.com>
2022-10-23 21:40:37 -0300

    Cambié un poco el diseño del footer y lo agregué a las demás páginas. También puse los íconos en las ventanas de cada página

--- js/headerfooter.js
+++ js/headerfooter.js
@@ -14,15 +14,16 @@ document.getElementById("idheader").innerHTML = header
 
 let footer = `
     
+<div class="container-pie-y-copyright">
     <div class="container-pie">
         
         <div class="item0-pie">
-            <h3 style="color: #E9EAEC; padding: 1vh 2.5vw">Unitarios & Federales</h4>    
+            <h4>Unitarios & Federales</h4>    
         </div>
             
         <div class="subcontenedor-pie">
                 
-            <div class="item1-pie" style="padding: 1.5vh 5vw">
+            <div class="item1-pie">
                 
                 <div> <h5>Dirección</h5>
                 <h5>3 de febrero 1852, Caseros, Argentina</h5> </div>
@@ -30,9 +31,9 @@ let footer = `
                 <div> <h5>Horario</h5>
                 <h5>12 a 24 hs</h5> </div>
                 
-                </div>
+            </div>
 
-            <div class="item2-pie" style="padding: 1.5vh 5vw">
+            <div class="item2-pie">
                 <h5><img src="/img/flaticon/telefono.png" width="5%"> 4321-1234</h5>
                 <h5><img src="/img/flaticon/whatsapp.png" width="5%"> 15-1234-4321</h5>
                 <h5><img src="/img/flaticon/facebook.png" width="5%"> @Unitarios&Federales</h5>
@@ -42,6 +43,12 @@ let footer = `
         </div>
 
     </div>
+    
+    <div class="copyright">
+        <small>&copy; 2022 <b>Unitarios & Federales</b> - Todos los Derechos Reservados </small>
+    </div>
+</div>
+    
 `
 
 document.getElementById("idfooter").innerHTML = footer
